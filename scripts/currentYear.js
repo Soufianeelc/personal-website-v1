8ffc02026ada2b0791b4c466a996243939632655
soufianeelc <K137108149@taalim.ma>
2022-09-01 21:21:54 +0100

    use localStorage for Dark mode

--- scripts/currentYear.js
+++ scripts/currentYear.js
@@ -12,19 +12,78 @@ const icon = document.getElementById('icon')
 
 localStorage.setItem("theme", "light")
 
-let theme = 'light';
+// let theme = 'light';
 
-btn.addEventListener('click',() => {
+// btn.addEventListener('click',() => {
      
-    if(theme === 'light'){
-        html.classList.add('dark')
-        theme = 'dark'
-        icon.src = './img/icons8-sun.svg'
-    }
-    else{
-        html.classList.remove('dark')
-        icon.src = './img/halfmoon.png'
-        theme = 'light'
+//     if(theme === 'light'){
+//         html.classList.add('dark')
+//         theme = 'dark'
+//         icon.src = './img/icons8-sun.svg'
+//     }
+//     else{
+//         html.classList.remove('dark')
+//         icon.src = './img/halfmoon.png'
+//         theme = 'light'
 
+//     }
+// })
+let darkMode = localStorage.getItem("dark-mode");
+
+const enableDarkMode = () => {
+    html.classList.add('dark')
+    icon.src = './img/icons8-sun.svg'
+    localStorage.setItem("dark-mode", "enabled");
+  };
+  
+  const disableDarkMode = () => {
+    html.classList.remove('dark')
+    icon.src = './img/halfmoon.png'
+    localStorage.setItem("dark-mode", "disabled");
+  };
+  
+  if (darkMode === "enabled") {
+    enableDarkMode(); // set state of darkMode on page load
+  }
+  
+  btn.addEventListener("click", (e) => {
+    darkMode = localStorage.getItem("dark-mode"); // update darkMode when clicked
+    if (darkMode === "disabled") {
+      enableDarkMode();
+    } else {
+      disableDarkMode();
     }
-})
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
